312dfaf5b5697f4f39c55853967023fdfaee3d4a
Esra Emine Evren <88592010+esraevren@users.noreply.github.com>
2023-02-01 13:33:33 +0300

    cards created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,130 @@
+import { ReactComponent as Check } from "./assets/Icon.svg";
+import { ReactComponent as Close } from "./assets/Icon (1).svg";
+import { ReactComponent as Logo } from "./assets/logo.svg";
+
 function App() {
   return (
-    <div className="grid grid-cols-3 gap-24 py-12 px-24 font-roboto ">
-      <div className="bg-black text-white ">
-        <h5>fixpack</h5>
-        <div>Koruma Paketi - 1</div>
-        <div>₺450,00</div>
+    <div className="grid grid-cols-3 py-12 ml-12  font-roboto ">
+      <div className=" w-96 ">
+        <div className="px-8 py-8">
+          <Logo />
+          <div className="mt-3 mb-3 text-gray-700 font-semibold ">Koruma Paketi - 1</div>
+          <div className="text-xl font-bold ">₺450,00</div>
+          <div className="flex mt-8">
+            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3 ">
+              <Check />
+            </div>
+            <div className="text-gray-700 font-medium">Ücretsiz Onarım</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Check />
+            </div>
+            <div className="text-gray-700 font-medium">Ücretsiz Onarım</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Check />
+            </div>
+            <div className="text-gray-700 font-medium">Ek 1 Yıl Garanti</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Close />
+            </div>
+            <div className="text-gray-500 ">Dahil Olmayan Özellik</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Close />
+            </div>
+            <div className="text-gray-500">Dahil Olmayan Özellik</div>
+          </div>
+          <button class="bg-[#0049E6]  text-white  py-3 px-5 w-[316px] rounded">
+          Planı Seç
+</button>
+        </div>
       </div>
-      <div className="bg-black">esra</div>
-      <div className="bg-black">esra</div>
+      <div className=" w-96 ">
+        <div className="px-8 py-8">
+          <Logo />
+          <div className="mt-3 mb-3">Koruma Paketi - 1</div>
+          <div className="text-xl font-bold ">₺450,00</div>
+          <div className="flex mt-8">
+            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3 ">
+              <Check />
+            </div>
+            <div>Ücretsiz Onarım</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Check />
+            </div>
+            <div>Ücretsiz Onarım</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Check />
+            </div>
+            <div>Ek 1 Yıl Garanti</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Close />
+            </div>
+            <div>Dahil Olmayan Özellik</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Close />
+            </div>
+            <div>Dahil Olmayan Özellik</div>
+          </div>
+          <button class="bg-[#0049E6] text-white  py-3 px-5 w-[316px] rounded">
+          Planı Seç
+</button>
+        </div>
+      </div>
+      <div className=" w-96 ">
+        <div className="px-8 py-8">
+          <Logo />
+          <div className="mt-3 mb-3">Koruma Paketi - 1</div>
+          <div className="text-xl font-bold ">₺450,00</div>
+          <div className="flex mt-8">
+            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3 ">
+              <Check />
+            </div>
+            <div>Ücretsiz Onarım</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Check />
+            </div>
+            <div>Ücretsiz Onarım</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Check />
+            </div>
+            <div>Ek 1 Yıl Garanti</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Close />
+            </div>
+            <div>Dahil Olmayan Özellik</div>
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+              <Close />
+            </div>
+            <div>Dahil Olmayan Özellik</div>
+          </div>
+          <button class="bg-[#0049E6] text-white  py-3 px-5 w-[316px] rounded">
+          Planı Seç
+</button>
+        </div>
+      </div>
     </div>
   );
 }
