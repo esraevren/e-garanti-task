b00962b3be8f838d1ab28fe351736cc663511e44
Esra Emine Evren <88592010+esraevren@users.noreply.github.com>
2023-02-01 11:56:58 +0300

    font changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
-
 function App() {
   return (
-    <div className="bg-black">
-  esra
+    <div className="grid grid-cols-3 gap-24 py-12 px-24 font-roboto ">
+      <div className="bg-black text-white ">
+        <h5>fixpack</h5>
+        <div>Koruma Paketi - 1</div>
+        <div>₺450,00</div>
+      </div>
+      <div className="bg-black">esra</div>
+      <div className="bg-black">esra</div>
     </div>
   );
 }
