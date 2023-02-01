708abbfe54217e9c603fe99feb9b7d16d72b3837
Esra Emine Evren <88592010+esraevren@users.noreply.github.com>
2023-02-01 16:42:27 +0300

    data fetched

--- src/App.js
+++ src/App.js
@@ -1,130 +1,101 @@
 import { ReactComponent as Check } from "./assets/Icon.svg";
 import { ReactComponent as Close } from "./assets/Icon (1).svg";
 import { ReactComponent as Logo } from "./assets/logo.svg";
+import datas from "./data";
 
 function App() {
   return (
     <div className="grid grid-cols-3 py-12 ml-12  font-roboto ">
-      <div className=" w-96 ">
-        <div className="px-8 py-8">
-          <Logo />
-          <div className="mt-3 mb-3 text-gray-700 font-semibold ">Koruma Paketi - 1</div>
-          <div className="text-xl font-bold ">₺450,00</div>
-          <div className="flex mt-8">
-            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3 ">
-              <Check />
+      {datas.map((data) => {
+        return (
+          <div className="w-96">
+            {data.suggest === true ? <div className="bg-blue-800 py-3 text-white flex items-center justify-center rounded-t-lg">Önerilen</div> : <div className=" py-3 text-white">"</div>}
+            <div className=" px-8 py-8">
+              
+              <Logo />
+              <div className="mt-3 mb-3 text-gray-700 font-semibold ">
+                {data.title}
+              </div>
+              <div className="text-xl font-bold ">₺{data.price},00</div>
+              <div className="flex mt-8">
+                {data.features[0].goodFeature === true ? (
+                  <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3 ">
+                    <Check />
+                  </div>
+                ) : (
+                  <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+                    <Close />
+                  </div>
+                )}
+                <div className="text-gray-700 font-medium">
+                  {data.features[0].title}
+                </div>
+              </div>
+
+              <div className="flex">
+                {data.features[1].goodFeature === true ? (
+                  <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3 ">
+                    <Check />
+                  </div>
+                ) : (
+                  <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+                    <Close />
+                  </div>
+                )}
+                <div className="text-gray-700 font-medium">
+                  {data.features[1].title}
+                </div>
+              </div>
+              <div className="flex">
+                {data.features[2].goodFeature === true ? (
+                  <>
+                    <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3 ">
+                      <Check />
+                    </div>
+                    <div className="text-gray-700 font-medium">
+                      {data.features[2].title}
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+                      <Close />
+                    </div>
+                    <div className="text-gray-500 ">
+                      {data.features[2].title}
+                    </div>
+                  </>
+                )}
+              </div>
+
+              <div className="flex">
+                {data.features[3].goodFeature === true ? (
+                  <>
+                    <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3 ">
+                      <Check />
+                    </div>
+                    <div className="text-gray-700 font-medium">
+                      {data.features[3].title}
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
+                      <Close />
+                    </div>
+                    <div className="text-gray-500 ">
+                      {data.features[3].title}
+                    </div>
+                  </>
+                )}
+              </div>
+              <button class="bg-[#0049E6] text-white  py-3 px-5 w-[316px] rounded">
+                Planı Seç
+              </button>
             </div>
-            <div className="text-gray-700 font-medium">Ücretsiz Onarım</div>
           </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Check />
-            </div>
-            <div className="text-gray-700 font-medium">Ücretsiz Onarım</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Check />
-            </div>
-            <div className="text-gray-700 font-medium">Ek 1 Yıl Garanti</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Close />
-            </div>
-            <div className="text-gray-500 ">Dahil Olmayan Özellik</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Close />
-            </div>
-            <div className="text-gray-500">Dahil Olmayan Özellik</div>
-          </div>
-          <button class="bg-[#0049E6]  text-white  py-3 px-5 w-[316px] rounded">
-          Planı Seç
-</button>
-        </div>
-      </div>
-      <div className=" w-96 ">
-        <div className="px-8 py-8">
-          <Logo />
-          <div className="mt-3 mb-3">Koruma Paketi - 1</div>
-          <div className="text-xl font-bold ">₺450,00</div>
-          <div className="flex mt-8">
-            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3 ">
-              <Check />
-            </div>
-            <div>Ücretsiz Onarım</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Check />
-            </div>
-            <div>Ücretsiz Onarım</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Check />
-            </div>
-            <div>Ek 1 Yıl Garanti</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Close />
-            </div>
-            <div>Dahil Olmayan Özellik</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Close />
-            </div>
-            <div>Dahil Olmayan Özellik</div>
-          </div>
-          <button class="bg-[#0049E6] text-white  py-3 px-5 w-[316px] rounded">
-          Planı Seç
-</button>
-        </div>
-      </div>
-      <div className=" w-96 ">
-        <div className="px-8 py-8">
-          <Logo />
-          <div className="mt-3 mb-3">Koruma Paketi - 1</div>
-          <div className="text-xl font-bold ">₺450,00</div>
-          <div className="flex mt-8">
-            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3 ">
-              <Check />
-            </div>
-            <div>Ücretsiz Onarım</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Check />
-            </div>
-            <div>Ücretsiz Onarım</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-[#AAC2F7]  text-blue-800 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Check />
-            </div>
-            <div>Ek 1 Yıl Garanti</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Close />
-            </div>
-            <div>Dahil Olmayan Özellik</div>
-          </div>
-          <div className="flex">
-            <div className="flex justify-center items-center select-none bg-gray-100  text-gray-500 text-xl font-bold   rounded-full  h-6 w-6 mr-3 mb-3">
-              <Close />
-            </div>
-            <div>Dahil Olmayan Özellik</div>
-          </div>
-          <button class="bg-[#0049E6] text-white  py-3 px-5 w-[316px] rounded">
-          Planı Seç
-</button>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,89 @@
+export default [
+  {
+    id: 1,
+    title: "Koruma Paketi - 1",
+    logo: "https://dstal.com.au/wp-content/uploads/2021/09/logoipsum.png",
+    suggest: false,
+    price: 100,
+    features: [
+      {
+        id: 1,
+        title: "Ücretsiz Onarıma",
+        goodFeature: true
+      },
+      {
+        id: 2,
+        title: "Ücretsiz Bakıma",
+        goodFeature: true
+      },
+      {
+        id: 3,
+        title: "Ücretsiz Yedek Parça",
+        goodFeature: false
+      },
+      {
+        id: 4,
+        title: "Ücretsiz Yedek Parça",
+        goodFeature: false
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "Koruma Paketi - 2",
+    logo: "https://dstal.com.au/wp-content/uploads/2021/09/logoipsum.png",
+    suggest: true,
+    price: 200,
+    features: [
+      {
+        id: 1,
+        title: "Ücretsiz Onarıma",
+        goodFeature: true
+      },
+      {
+        id: 2,
+        title: "Ücretsiz Bakıma",
+        goodFeature: true
+      },
+      {
+        id: 3,
+        title: "Ücretsiz Yedek Parça",
+        goodFeature: true
+      },
+      {
+        id: 4,
+        title: "Ücretsiz Yedek Parça",
+        goodFeature: true
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "Koruma Paketi - 3",
+    logo: "https://dstal.com.au/wp-content/uploads/2021/09/logoipsum.png",
+    suggest: false,
+    price: 300,
+    features: [
+      {
+        id: 1,
+        title: "Ücretsiz Onarıma",
+        goodFeature: true
+      },
+      {
+        id: 2,
+        title: "Ücretsiz Bakıma",
+        goodFeature: true
+      },
+      {
+        id: 3,
+        title: "Ücretsiz Yedek Parça",
+        goodFeature: true
+      },
+      {
+        id: 4,
+        title: "Ücretsiz Yedek Parça",
+        goodFeature: false
+      }
+    ]
+  }
+];
